fc3dbc2250663e9ff38d93eddd3ea26e11e0e64a
apple <apple@macdeMacBook-Pro-2.local>
2019-03-08 12:34:21 +0000

    3

--- src/Utils/InfixToSuffix.java
+++ src/Utils/InfixToSuffix.java
@@ -86,7 +86,6 @@ public class InfixToSuffix {
 		String[] arr = equation.split(","); // 根据, 拆分字符串
 		List<String> list = new ArrayList<String>(); // 用于计算时 存储运算过程的集合【例如list中当前放置 100 20 5 / 则取出20/5 最终将结果4存入list
 														// 此时list中结果为 100 4 】
-
 		for (int i = 0; i < arr.length; i++) { // 此处就是上面说的运算过程， 因为list.remove的缘故，所以取出最后一个数个最后两个数 都是size-2
 			int size = list.size();
 			switch (arr[i]) {
