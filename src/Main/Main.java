cf0ea9e49a583e5953cad591ee0754d54ab192a9
apple <apple@macdeMacBook-Pro-2.local>
2019-03-08 12:28:17 +0000

    初始化

---
+++ src/Main/Main.java
@@ -0,0 +1,18 @@
+package Main;
+
+import Utils.InfixToSuffix;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		test();
+	}
+
+	public static void test() {
+		String a = InfixToSuffix.toSuffix("55.6+4*60/2+33+(4.7*5.6)/5");// 传入 一串 算数公式
+		System.out.println(a);
+
+	}
+
+}
